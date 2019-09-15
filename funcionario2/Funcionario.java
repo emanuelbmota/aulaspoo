f1967b0563cf13d7d94eb83d6c859ef8b461938e
emanuelbmota <emanuel.mota03@gmail.com>
2019-09-15 14:54:03 -0300

    domingsss commit

---
+++ funcionario2/Funcionario.java
@@ -0,0 +1,26 @@
+public class Funcionario {
+    
+    private int codFUncionario;
+    public String nomeFuncionario;
+    protected int foneFuncionario;
+
+    Funcionario(String nomeFuncionario, int foneFuncionario){
+
+        codFUncionario = 0;
+        this.foneFuncionario = foneFuncionario;
+        this.nomeFuncionario = nomeFuncionario;
+
+    }
+
+    //método registrar ponto
+    public boolean registarPonto(){
+
+        return(true);
+    }
+    
+    public int Codfuncionario(){
+        
+        return(this.codFUncionario);
+        
+    }
+}
