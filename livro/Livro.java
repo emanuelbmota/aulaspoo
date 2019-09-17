085f5676dbe678b2792a4ce2a965f0f7458d3dd5
emanuelbmota <emanuel.mota03@gmail.com>
2019-09-16 22:42:56 -0300

    classe livro

---
+++ livro/Livro.java
@@ -0,0 +1,29 @@
+public class Livro {
+    
+    String titulo;
+    int paginaAtual;
+    int numPaginas;
+    
+    Livro(String titulo, int numPaginas, int paginaAtual){
+        
+        this.titulo = titulo;
+        this.paginaAtual = paginaAtual;
+        this.numPaginas = numPaginas;
+    }
+    
+    void avancarPagina(){
+        if(this.paginaAtual < this.numPaginas){
+            this.paginaAtual += 1;
+        }
+    }
+    
+    void voltarPagina(){
+        if(this.paginaAtual <= this.numPaginas && this.paginaAtual >= 2){
+            this.paginaAtual -= 1;
+        }
+    }
+    
+    int paginaAtual(){
+        return(this.paginaAtual);
+    }
+}
