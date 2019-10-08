4a2eed9b453598ff747a6031c3a1b3f57c0ad95d
emanuelbmota <emanuel.mota03@gmail.com>
2019-10-08 20:56:35 -0300

    classe carro

--- carro/Carro.java
+++ carro/Carro.java
@@ -15,8 +15,7 @@ public class Carro {
     }
     
     public void encheTanque(float litros){
-        
-        if (litros <= this.capacidadeTanque && litros <= this.volumeTanque && litros != 0){
+        if (this.capacidadeTanque >= (this.volumeTanque + litros) && litros != 0 && litros > 0){
             this.volumeTanque += litros;
             System.out.println("Abastecimento realizado.\nVolume no tanque(l): " + this.volumeTanque);
         }
@@ -28,9 +27,9 @@ public class Carro {
     
     public void passeio(float km){
         
-        if ((this.volumeTanque * this.rendimento) > km && km != 0){
-           this.volumeTanque = (this.rendimento * this.volumeTanque) - km;
-            System.out.println("Passeio será realizado com segurança!\n Volume no tanque após passeio (l): " + this.volumeTanque);
+        if ((this.volumeTanque * this.rendimento) > km && km != 0 && km > 0){
+            float resultado = this.volumeTanque * this.rendimento - km;
+            System.out.println("Passeio será realizado com segurança!\n Volume no tanque após passeio (l): " + resultado);
         }
         else{
             System.out.println("Não há combustível suficiente para realizar o passeio!\nVolume no tanque (l): "+ this.volumeTanque );
--- carro/TesteCarro.java
+++ carro/TesteCarro.java
@@ -4,10 +4,10 @@ public class TesteCarro {
 
     public static void main(String[] args) {
         
-        Carro c1 = new Carro(50.0f, 50.0f, 2.0f);
-        c1.passeio(100);
+        Carro c1 = new Carro(50.0f, 40.0f, 1.0f);
+        c1.passeio(99);
         System.out.println("\n");
-        c1.encheTanque(10);
+        c1.encheTanque(11);
         System.out.println("\n");
         c1.passeio(50);
         System.out.println("\n");
