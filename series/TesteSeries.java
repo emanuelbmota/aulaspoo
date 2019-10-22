ca5e73e1c27b88f2a0733162fc103908a138f1a7
emanuelbmota <emanuel.mota03@gmail.com>
2019-10-21 23:45:13 -0300

    correção serie

--- series/TesteSeries.java
+++ series/TesteSeries.java
@@ -10,15 +10,15 @@ public class TesteSeries{
         System.out.println(s1.mostraSerie());
 
         Series s2 = new Series();
-        s1.setI1(10);
-        s1.setI2(0);
-        s1.setPasso(2);
+        s2.setI1(10);
+        s2.setI2(0);
+        s2.setPasso(2);
         System.out.println(s1.mostraSerie());
 
         Series s3 = new Series();
-        s1.setI1(-5);
-        s1.setI2(5);
-        s1.setPasso(1);
+        s3.setI1(-5);
+        s3.setI2(5);
+        s3.setPasso(1);
         System.out.println(s1.mostraSerie());
     }
 }
