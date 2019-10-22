e85933f68d9113a9cbc08532cd8f23a5b2f707c6
emanuelbmota <emanuel.mota03@gmail.com>
2019-10-21 21:10:22 -0300

    series

---
+++ series/TesteSeries.java
@@ -0,0 +1,12 @@
+
+public class TesteSeries{
+
+    public static void main(String[] args){
+
+        Series s1 = new Series();
+        s1.setI1(-5);
+        s1.setI2(5);
+        s1.setPasso(0);
+        System.out.println(s1.mostraSerie());
+    }
+}
